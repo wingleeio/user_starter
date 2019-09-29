316e9dddc627b2907e1d95d50dea18e5f531b1fc
wingleeio <contact@winglee.io>
2019-09-29 15:45:41 -0700

    Update Cors.php

--- app/Http/Middleware/Cors.php
+++ app/Http/Middleware/Cors.php
@@ -17,7 +17,6 @@ class Cors
     {
         return $next($request)
             ->header('Access-Control-Allow-Origin', 'localhost')
-            ->header('Access-Control-Allow-Origin', 'mantou-buns.netlify.com')
             ->header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, OPTIONS');
     }
 }
